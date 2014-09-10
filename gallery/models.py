10a8f6bec7fe361f05ac88ea88a26682878b3603
Your Name Here <your_email@youremail.com>
2014-09-10 11:51:02 +0600

    Models are added

---
+++ gallery/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from pages.models import Page
+# Create your models here.
+
+class Gallery(models.Model):
+    page_id = models.ForeignKey(Page)
+    query = models.CharField( max_length = 25 )
+    from_date = models.DateTimeField( null = True , blank = True)
+    to_date = models.DateTimeField( null = True , blank = True )
+    page_size = models.IntegerField( null = True, blank = True )
+
+    class Meta:
+        db_table = 'galleries'
