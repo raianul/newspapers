c8c393558ae06e1570e4bf0d51daa0f86e60b55b
Your Name Here <your_email@youremail.com>
2014-09-10 15:36:49 +0600

    mages added

---
+++ images/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from images.models import Images
+from pages.models import Page
+# Register your models here.
+
+admin.site.register(Images)
+
--- newspapers/settings.py
+++ newspapers/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages',
-    'gallery'
+    'gallery',
+    'images'
 )
 
 MIDDLEWARE_CLASSES = (
